8f05d624cb28390501348aa8c00c79fbb5d3108c
Wesley Gado <wesleygado@gmail.com>
2024-02-02 15:46:37 -0300

    alterações finais

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
     }),
   );
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
